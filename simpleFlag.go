13585d934304b9e6c55170e1e8f20fef91675191
Your Name <you@example.com>
2022-08-12 10:33:36 +0400

    testler

---
+++ simpleFlag.go
@@ -0,0 +1,21 @@
+/* package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+
+
+
+func main() {
+minusK:=flag.Bool("k",true,"k flag")
+minusO:=flag.Int("o", 1, "o flag")
+flag.Parse()
+valk:=*minusK
+valo:=*minusO
+valo++
+fmt.Println("k:=>", valk)
+fmt.Println("o:=>", valo)
+	
+} */
