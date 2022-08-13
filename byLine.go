bda8ed4e42e317149dc5b853575f64a7c1b9b1d8
Your Name <you@example.com>
2022-08-13 11:33:23 +0400

    soz be soz oxumaq

--- byLine.go
+++ byLine.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func lineByLine(file string) error {
@@ -30,14 +31,49 @@ func lineByLine(file string) error {
 	return nil
 }
 
+func wordByWord(file string) error{
+	var err error
+	f, err := os.Open(file)
+	if err!=nil{
+		fmt.Println("burada bese xeta oldu", err)
+	}
+	defer f.Close()
+
+	r:=bufio.NewReader(f)
+
+	for {
+		line, err :=r.ReadString('\n')
+		if err == io.EOF{
+			break
+		} else if err != nil{
+			fmt.Println(err)
+		}
+		arr:=strings.Split(line, " ")
+
+		for _, v := range arr{
+			r:=strings.TrimSpace(v)
+			if r =="" {
+				continue
+			}
+			fmt.Println(r)
+		}
+
+	}
+
+
+
+	return nil
+
+}
+
 func main() {
 	flag.Parse()
 	if len(flag.Args()) == 0 {
-		fmt.Printf("istifadə qaydasi: byLine <fayl1> <file2>\n")
+		fmt.Printf("istifadə qaydasi: byWord <fayl1> <file2>\n")
 		return
 	}
 	for _, file := range flag.Args() {
-		err := lineByLine(file)
+		err := wordByWord(file)
 
 		if err != nil {
 			fmt.Println(err)
